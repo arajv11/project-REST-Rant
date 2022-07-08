b65c0bba7344093e5df608b22e353da995eb7fa9
Anish Raj <a.rajv11@gmail.com>
2022-07-07 21:35:28 -0400

    Adding Bootstrap grid to index page

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,25 @@
 const router = require('express').Router()
+const express = require('express')
+const app = express()
 
-router.get('/', (req, res) => {
-    res.send('GET /places')
+// GET /places
+app.get('/', (req, res) => {
+    let places = [{
+        name: 'H-Thai-ML',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Thai, Pan-Asian',
+        pic: '/images/h-thai-ml-tables.jpg'
+        // Image by <a href="https://pixabay.com/users/kaboompics-1013994/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=791167">Karolina Grabowska</a> from <a href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=791167">Pixabay</a>
+    }, {
+        name: 'Coding Cat Cafe',
+        city: 'Phoenix',
+        state: 'AZ',
+        cuisines: 'Coffee, Bakery',
+        pic: '/images/coffee-cat.jpg'
+        // Image by <a href="https://pixabay.com/users/sharonang-99559/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=1711012">Sharon Ang</a> from <a href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=1711012">Pixabay</a>
+    }]      
+    res.render('places/index', { places })
 })
 
 module.exports = router
