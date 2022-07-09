60e6e7b55d832c260f03540766e0171f9fd1e039
Anish Raj <a.rajv11@gmail.com>
2022-07-08 23:37:40 -0400

    commit

--- controllers/places.js
+++ controllers/places.js
@@ -3,35 +3,18 @@ const express = require('express')
 const app = express()
 const places = require('../models/places.js')
 
-// GET /places
-app.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/h-thai-ml-tables.jpg'
-        // Image by <a href="https://pixabay.com/users/kaboompics-1013994/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=791167">Karolina Grabowska</a> from <a href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=791167">Pixabay</a>
-    }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/coffee-cat.jpg'
-        // Image by <a href="https://pixabay.com/users/sharonang-99559/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=1711012">Sharon Ang</a> from <a href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=1711012">Pixabay</a>
-    }]      
+router.get('/', (req, res) => {
     res.render('places/index', { places })
 })
 
 router.get('/new', (req, res) => {
-    console.log('/new called')
     res.render('places/new')
 })
 
 router.post('/', (req, res) => {
-    if (!req.body.pic) {
+    if (!req.body.img) {
         // Default image if one is not provided
-        req.body.pic = 'http://placekitten.com/400/400'
+        req.body.img = 'http://placekitten.com/400/400'
     }
     if (!req.body.city) {
         req.body.city = 'Anytown'
