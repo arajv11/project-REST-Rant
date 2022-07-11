fc1f03c6c969e94f80d771f442ac8c18fdb48cb7
Anish Raj <a.rajv11@gmail.com>
2022-07-11 00:31:22 -0400

    Added the edit functionality

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -4,22 +4,22 @@ const Def = require('../default')
 function show (data) {
     return (
         <Def>
-          <main class="row bg-warning add-margin">
-            <h1 class="text-info bg-danger add-padding">{ data.place.name }</h1>
+          <main className="row bg-warning add-margin">
+            <h1 className="text-info bg-danger add-padding">{ data.place.name }</h1>
             <br/>
             <br/>
-            <div class="row col-6">
-              <h2 class="text-info bg-secondary add-padding">Rating</h2>
+            <div className="row col-6">
+              <h2 className="text-info bg-secondary add-padding">Rating</h2>
               <h3>currently unrated</h3>
               <br/>
-              <h2 class="text-info bg-secondary add-padding">Description</h2>
+              <h2 className="text-info bg-secondary add-padding">Description</h2>
               <h3>Located in { data.place.city }, { data.place.state }</h3>
               <h3>Cuisines: { data.place.cuisines }</h3>
             </div>
-            <div class="col-6">
-              <img src={ data.place.pic } alt={ data.place.name } class="col-6"/>
+            <div className="col-6">
+              <img src={ data.place.pic } alt={ data.place.name } className="col-6"/>
             </div>
-            <h2 class="text-info bg-secondary add-padding">Comments</h2>
+            <h2 className="text-info bg-secondary add-padding">Comments</h2>
             <h3>No comments yet!</h3>
             <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
               Edit
