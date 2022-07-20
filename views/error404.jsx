c160021e1a23acff1276aa93959a114198afcab9
Anish Raj <a.rajv11@gmail.com>
2022-07-20 15:51:35 -0400

    Part 10

--- views/error404.jsx
+++ views/error404.jsx
@@ -8,7 +8,7 @@ function error404 () {
                 <h1>404: PAGE NOT FOUND</h1>
                 <p>Oops, sorry, we can't find this page!</p>
                 <div>
-                    <img src="/images/flatlay.jpg" alt="Flatlay" />
+                    <img src="/images/flatlay.jpg" alt="Flatlay" style={{width:"100%"}}/>
                     <div>
                         Photo by <a href="https://unsplash.com/@cravethebenefits?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Brenda Godinez</a> on <a href="https://unsplash.com/@cravethebenefits?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
                     </div>
